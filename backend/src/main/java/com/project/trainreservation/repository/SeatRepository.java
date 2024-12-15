d02ddc3e70e1fd7b94e2a35aa6eaab01c9e5de5c
Elif Yağmur Bölük <78900224+yagmurboluk@users.noreply.github.com>
2024-12-15 16:16:49 +0300

    SeatRepository.java

--- backend/src/main/java/com/project/trainreservation/repository/SeatRepository.java
+++ backend/src/main/java/com/project/trainreservation/repository/SeatRepository.java
@@ -9,36 +9,14 @@ import java.util.List;
 @Repository
 public interface SeatRepository extends JpaRepository<SeatEntity, Long> {
 
-    /**
-     * Bir trene ait tüm koltukları getirir.
-     *
-     * @param trainId Trenin ID'si.
-     * @return Trene ait tüm koltukların listesi.
-     */
     List<SeatEntity> findByTrain_TrainId(Long trainId);
 
-    /**
-     * Bir trene ait rezerve edilmemiş koltukları getirir.
-     *
-     * @param trainId Trenin ID'si.
-     * @return Trene ait rezerve edilmemiş koltukların listesi.
-     */
+    
     List<SeatEntity> findByTrain_TrainIdAndReservedFalse(Long trainId);
 
-    /**
-     * Bir trene ait rezerve edilmiş koltukları getirir.
-     *
-     * @param trainId Trenin ID'si.
-     * @return Trene ait rezerve edilmiş koltukların listesi.
-     */
+    
     List<SeatEntity> findByTrain_TrainIdAndReservedTrue(Long trainId);
 
-    /**
-     * Belirli bir koltuk numarasına göre koltuğu getirir.
-     *
-     * @param trainId Trenin ID'si.
-     * @param seatNumber Koltuk numarası.
-     * @return Belirli bir koltuğun bilgisi.
-     */
+    
     SeatEntity findByTrain_TrainIdAndSeatNumber(Long trainId, int seatNumber);
 }
