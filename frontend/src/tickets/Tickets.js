49d6bd556151097bbe10ce0da967174bfdf77633
Elif Yağmur Bölük <78900224+yagmurboluk@users.noreply.github.com>
2024-12-15 16:33:42 +0300

    Tickets.js

--- frontend/src/tickets/Tickets.js
+++ frontend/src/tickets/Tickets.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { QRCodeCanvas } from "qrcode.react"; // QR Kod Oluşturucu
+import { QRCodeCanvas } from "qrcode.react"; 
 import "./Tickets.css";
 
 const Tickets = () => {
